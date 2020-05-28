59bde27c6109b9520d399ea5a2e77ae303f8d720
natanaelkristiawan <natanaelkristiawan@hotmail.com>
2020-05-28 10:02:16 +0800

    update

--- module/site/resources/views/public/details/location.blade.php
+++ module/site/resources/views/public/details/location.blade.php
@@ -21,30 +21,64 @@
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD6hoGyKmhX5TUFJB9ucNsNvb-wm8wZxfI&libraries=places"></script>
 <script src="{{ asset('themes/additionals/gmaps') }}/gmaps.js" type="text/javascript"></script>
 <script type="text/javascript">
-  function initMap(zoom, data) {
-    if ($('#map').length == 1) {
-      if (typeof zoom == 'undefined') {
-        zoom = 15;
-      }
 
-      latest_lat = $('#map').data('latitude');
-      latest_lng = $('#map').data('longitude');
+ function initMap() {
+    latest_lat = $('#map').data('latitude');
+    latest_lng = $('#map').data('longitude');
+    // Create the map.
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: {lat: latest_lat, lng: latest_lng},
+      mapTypeId: 'terrain'
+    });
 
-      map = new GMaps({
-        div: '#map',
-        lat: latest_lat,
-        lng: latest_lng,
-        zoom: 14,
-        textSearch: true
-      });
-
-      map.addMarker({
-        lat: latest_lat,
-        lng: latest_lng,
-        draggable: false
-      });
-    }
+    // Construct the circle for each value in citymap.
+    // Note: We scale the area of the circle based on the population.
+  
+    // Add the circle for this city to the map.
+    new google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: {
+        lat : latest_lat,
+        lng : latest_lng
+      },
+      radius:  100
+    });
+ 
   }
+
+  // function initMap(zoom, data) {
+  //   if ($('#map').length == 1) {
+  //     if (typeof zoom == 'undefined') {
+  //       zoom = 15;
+  //     }
+
+  //     latest_lat = $('#map').data('latitude');
+  //     latest_lng = $('#map').data('longitude');
+
+  //     map = new GMaps({
+  //       div: '#map',
+  //       lat: latest_lat,
+  //       lng: latest_lng,
+  //       zoom: 14,
+  //       textSearch: true
+  //     });
+
+
+
+
+  //     map.addMarker({
+  //       lat: latest_lat,
+  //       lng: latest_lng,
+  //       draggable: false
+  //     });
+  //   }
+  // }
 
   $(document).ready(function(){
     initMap();

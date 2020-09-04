09fbddb502f847386951860fd84cb822903ed710
natanaelkristiawan <natanaelkristiawan@hotmail.com>
2020-09-04 08:43:44 +0800

    update

--- master/rooms/src/Repositories/Presenter/RoomsTransformer.php
+++ master/rooms/src/Repositories/Presenter/RoomsTransformer.php
@@ -13,7 +13,7 @@ class RoomsTransformer extends TransformerAbstract
       'photo' => '<a href="#" data-featherlight="'.url('image/original/'.$model->photo_primary).'"><img style="width:100px;border-radius:5px;" class="img-fluid"  src="'.url('image/profile/'.$model->photo_primary).'" /></a>',
       'name' => $model->name,
       'owner' => $model->owner->name,
-      'type' => $model->type->name,
+      'type' => is_null($model->type) ? '' : $model->type->name,
       'location' => $model->location->name,
       'address' => $model->address,
       'status'=> $model->status == 0 ? 'Draft' : 'Live',

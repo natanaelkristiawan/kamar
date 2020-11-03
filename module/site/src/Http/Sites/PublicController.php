da058f960f51282380f987ab6c0b9f277355b773
natanaelkristiawan <natanaelkristiawan@hotmail.com>
2020-11-04 06:19:34 +0800

    update

--- module/site/src/Http/Sites/PublicController.php
+++ module/site/src/Http/Sites/PublicController.php
@@ -175,7 +175,7 @@ class PublicController extends Controller
       $package->used_quota = $package->used_quota + 1;
       $package->remaining_quota = $package->remaining_quota - 1;
 
-      if (($package->remaining_quota - 1) == 0) {
+      if (($package->remaining_quota) == 0) {
         $package->status = 3;
       }
 

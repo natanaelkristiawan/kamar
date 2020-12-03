303cef9135dc301e145d8fe6ba3be334076e9e45
natanaelkristiawan <natanaelkristiawan@hotmail.com>
2020-12-03 14:10:20 +0800

    update

--- master/packages/src/Repositories/Presenter/CounterTransformer.php
+++ master/packages/src/Repositories/Presenter/CounterTransformer.php
@@ -8,13 +8,26 @@ class CounterTransformer extends TransformerAbstract
 {
 	public function transform(\Master\Packages\Models\Counter $model)
 	{
+
+		$room_id = null;
+		$owner_id = null;
+
+		if (count($model->room)) {
+			$room = $model->room->name
+		}
+
+		if (count($model->owner_id)) {
+			$$owner_id = $model->owner->name;
+		}
+
+
 		return [
 			'id'   	=> $model->id,
 			'created_at' => date('Y-m-d', strtotime($model->created_at)),
 			'ip'	=> $model->ip,
 			'fingerprint' => $model->fingerprint,
-			'room_id' => $model->room->name,
-			'owner_id' => $model->owner->name,
+			'room_id' => $room_id,
+			'owner_id' => $owner_id,
 		];
 	}
 }
